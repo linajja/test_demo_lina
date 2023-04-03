d09fdd661d70871c2f0c4ab70e6c39f2759773e0
linajja <lina.github@gmail.com>
2023-04-03 20:51:25 +0300

    Add Common class

---
+++ src/test/java/lt/lina/pom/pages/Common.java
@@ -0,0 +1,43 @@
+package lt.lina.pom.pages;
+
+import lt.lina.pom.utilies.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Common {
+    public static void setUpDriver() {
+        Driver.setDriver();
+    }
+
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
+    }
+
+    public static void closeDriver() {
+        Driver.closeDriver();
+    }
+
+    private static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
+    }
+
+    private static List<WebElement> getElements(By locator) {
+        return Driver.getDriver().findElements(locator);
+    }
+
+    public static void sendKeysToElement(By locator, String sendKeys) {
+
+        getElement(locator).sendKeys(sendKeys);
+    }
+
+    public static void clickOnElement(By locator) {
+        getElement(locator).click();
+    }
+
+    public static String getTextFromElement(By locator) {
+        return getElement(locator).getText();
+
+    }
+}
